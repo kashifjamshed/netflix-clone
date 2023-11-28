526886a8cc9ac6a3c0070ce72f74ea94d44a0f60
Kashif Jamshed <talktokashiif@gmail.com>
2023-11-28 23:51:36 +0530

    Some more style Added

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -12,15 +12,13 @@ function App() {
      <Row title= "NETFLIX ORIGINALS" 
      fetchUrl = {requests.fetchNetflixOriginals}
       isLargeRow/>
-     <Row title= "Trending Now" fetchUrl = {requests.fetchActionMovies}/>
+     <Row title= "Trending Now" fetchUrl = {requests.fetchTrending}/>
      <Row title= "Top Rated" fetchUrl = {requests.fetchTopRated}/>
      <Row title= "Action Movies" fetchUrl = {requests.fetchActionMovies}/>
      <Row title= "Comedy Movies" fetchUrl = {requests.fetchComedyMovies}/>
      <Row title= "Horror Movies" fetchUrl = {requests.fetchHorrorMovies}/>
-     <Row title= "Romance Movies" fetchUrl = {requests.fetchHorrorMovies}/>
+     <Row title= "Romance Movies" fetchUrl = {requests.fetchRomanceMovies}/>
      <Row title= "Documentries" fetchUrl = {requests.fetchDocumentaries}/>
-     
-    
     </div>
   );
 }
